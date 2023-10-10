8143e04a1b663816d9a950737575629f7003b87c
sui <1202ayasnppk@gmail.com>
2023-10-10 21:05:51 +0900

    add: ログイン後の画面を作成

---
+++ src/app/main/page.tsx
@@ -0,0 +1,16 @@
+import BookTitleForm from '../components/BookTitleForm';
+
+export default function Home() {
+  return (
+    <main>
+      <div className='flex flex-col items-center  min-h-screen py-4 bg-gray-200 ml-4 mr-4'>
+        <h1 className='text-3xl font-bold text-gray-700 mt-12 '>Personal BookNotion</h1>
+        <div className='w-full max-w-xl mt-8'>
+          <div className='w-full px-8 py-6 bg-white shadow-md rounded-lg'>
+            <BookTitleForm />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
