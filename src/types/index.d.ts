88ff9d9f43e1503e1e3a4fc32dc3f1eec9e0b80a
sui <1202ayasnppk@gmail.com>
2023-10-13 08:48:58 +0900

    add :type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,9 @@ interface BookProperties {
   highlights: string[];
   comments: string[];
 }
+
+interface PopupProps {
+  message: string;
+  type: 'success' | 'error' | null;
+  onClose: () => void;
+}
